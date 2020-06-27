5cc28e1c566f306a535ed8f83ee3b616b71a5251
Swapnil Golegaonkar <swapnilgolegaonkar@gmail.com>
2020-06-27 12:31:56 +0530

    Adding batch files

--- BuildAndRun.bat
+++ BuildAndRun.bat
@@ -2,7 +2,7 @@
 cd build 
 cmake .. -G "Visual Studio 16 2019" 
 cmake --build . --config Release 
-.\bin\calc_test.exe --gtest_output="xml:report.xml"
+REM .\bin\calc_test.exe --gtest_output="xml:report.xml"
 
 REM Inspect your current project’s dependencies:conan info ..
 REM Generate a dependencies graph for the current project: conan info .. --graph=graph.html
